9a267ccea8bde5aa7a1ddc2574a652dd19f400ca
Jacob Henry <jchenry@wpi.edu>
2019-01-31 21:28:49 -0500

    Now works

--- folderizer.py
+++ folderizer.py
@@ -5,8 +5,18 @@ FILE_SEP = ";FSEP;"
 ELT_SEP = ";ESEP;"
 PATH_SEP = ";PSEP;"
 
+INPUT_FOLDER = "download_zone"
+OUTPUT_FOLDER = "output"
+
 # Get the manifest
 manifest = pyperclip.paste()
+
+# Check that clipboard is valid
+if ELT_SEP not in manifest:
+    print("You messed up clipboard. Go back to canvas files and it should fix itself")
+    exit(1)
+
+# Start splitting
 file_entries = manifest.split(FILE_SEP)
 file_entries = [f.split(ELT_SEP) for f in file_entries]
 
@@ -14,17 +24,23 @@ file_entries = [f.split(ELT_SEP) for f in file_entries]
 file_entry_dict = {}
 for full_path, auto_name, indiv_name in file_entries:
     # Make the path a proper path
-    dir_path = full_path.split(PATH_SEP)[:-1].join(os.path.sep)
+    dir_path = os.path.join(OUTPUT_FOLDER, *(full_path.split(PATH_SEP)[:-1]))
     try:
         os.mkdir(dir_path)
     except OSError:
         pass # Doesn't matter
 
     # Find the file we're currently looking at
-    for candidate in os.listdir(os.getcwd()):
+    success = False
+    for candidate in os.listdir(INPUT_FOLDER):
         if auto_name in candidate:
             # That's our mark!
-            os.rename(candidate, os.path.join(dir_path, indiv_name))
+            os.rename(os.path.join(INPUT_FOLDER, candidate), os.path.join(dir_path, indiv_name))
+            success = True
             break
 
+    if not success:
+        # print("Could not find {} in {}".format(auto_name, INPUT_FOLDER))
+        pass
 
+print("Done")
