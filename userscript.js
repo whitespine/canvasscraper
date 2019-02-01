9ba098721e995f2f63545545a7428d11d145652f
Jacob Henry <jchenry@wpi.edu>
2019-01-31 21:31:55 -0500

    credited myself

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Steal scholastics shit.
-// @author       You
+// @author       Jacob Henry (github.com/whitespine)
 // @match        https://canvas.wpi.edu/files*
 // @grant        GM_getValue
 // @grant        GM_setValue
